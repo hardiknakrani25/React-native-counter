c97a544f85ee655bf49e9a7812f62b431a7ed84c
Hardik <hdnakarani@gmail.com>
2019-03-29 19:14:44 +0530

    create server for api request

--- src/Component/Home/dashbord.js
+++ src/Component/Home/dashbord.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import { SafeAreaView, Text } from "react-native";
 
 export default class Dashbord extends Component {
+  static navigationOptions = {
+    header: null
+  };
   render() {
     return (
       <SafeAreaView
