eb2197127a30d52ff1e367d5ef93279d203fbb2e
Hardik <hdnakarani@gmail.com>
2019-03-29 16:55:33 +0530

    add style.js

--- src/Component/Home/home.js
+++ src/Component/Home/home.js
@@ -1,19 +1,26 @@
 import React,{Component} from 'react';
-import {SafeAreaView,TextInput,View} from 'react-native';
+import {SafeAreaView,TextInput,View,Text,Button} from 'react-native';
+import styles from './styles';
 
 export default class Home extends Component{
     render(){
         return(
-            <SafeAreaView>
+            <SafeAreaView style={{justifyContent: 'center',flex: 1}}>
 <View>
+<Text style={styles.heading}>Login</Text>
 <TextInput
+style={[styles.input,{marginTop: 20}]}
                 placeholder={'Enter name'}
                 />
-                <TextInput
-                placeholder={'Enter Passsword'}/>
-</View>
-             
-            </SafeAreaView>
+<TextInput
+style={[styles.input,{marginTop: 20}]}
+secureTextEntry={true}
+placeholder={'Enter Passsword'}/>
+
+<Button title={'Login'}/>
+</View>             
+
+</SafeAreaView>
         )
     }
 }
